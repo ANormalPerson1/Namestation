e720ed635d2950c1b1e1a1608c8a870547753b47
Juš Dolžan <jus1671@gmail.com>
2021-10-31 23:02:15 +0100

    Started work on networking integration

---
+++ Project Namestation/Assets/Scripts/Player/MovementManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace Namestation.Player
+{
+    public class MovementManager : NetworkBehaviour
+    {
+        private void Update()
+        {
+            HandleMovement();
+        }
+
+        void HandleMovement()
+        {
+            if(isLocalPlayer)
+            {
+                Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f);
+                transform.position += movement * Time.deltaTime * 5f;
+            }
+        }
+    }
+}
+
