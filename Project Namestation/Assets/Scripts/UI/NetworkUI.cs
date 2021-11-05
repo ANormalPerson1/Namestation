32d29df78690e1c7403b6117e46bdfc9b258bc91
Juš Dolžan <jus1671@gmail.com>
2021-11-05 15:39:32 +0100

    Designed the join game menu

---
+++ Project Namestation/Assets/Scripts/UI/NetworkUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using TMPro;
+
+[RequireComponent(typeof(NetworkManager))]
+public class NetworkUI : MonoBehaviour
+{
+    [SerializeField] TMP_InputField ipField;
+    [SerializeField] TMP_InputField usernameField;
+    [SerializeField] TMP_InputField passwordField;
+
+    NetworkManager networkManager;
+
+    public void Host()
+    {
+        networkManager.StartHost();
+    }
+
+    public void Connect()
+    {
+        networkManager.networkAddress = ipField.text;
+        networkManager.StartClient();
+    }
+}
