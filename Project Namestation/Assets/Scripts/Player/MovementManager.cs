d1c8822459fcf54329284be22d02a016271de782
Juš Dolžan <jus1671@gmail.com>
2021-11-27 15:26:15 +0100

    Added first iteration of grid movement
    TODO: Rework, so all functionality is locally handled.

--- Project Namestation/Assets/Scripts/Player/MovementManager.cs
+++ Project Namestation/Assets/Scripts/Player/MovementManager.cs
@@ -10,8 +10,10 @@ namespace Namestation.Player
         [SerializeField] float movementSpeed;
         [SerializeField] Transform playerModelTransform;
         [SerializeField] Rigidbody2D playerRigidbody;
+        [SerializeField] LayerMask constructionLayermask;
         InputManager inputManager;
         Transform currentGrid;
+        Rigidbody2D currentGridRigidbody2D;
 
         public override void Initialize()
         {
@@ -24,13 +26,52 @@ namespace Namestation.Player
             if (!initialized || !isLocalPlayer) return;
             HandleMovement();
             HandleRotation();
+            CheckOverlap();
+            TEMP_HandleGridMovement();
+        }
+
+        void CheckOverlap()
+        {
+            Collider2D collider = Physics2D.OverlapCircle(playerRigidbody.position, 1f, constructionLayermask);
+
+            if (collider == null)
+            {
+                currentGrid = null;
+                currentGridRigidbody2D = null;
+            }
+            else
+            {
+                currentGrid = collider.transform.parent.parent;
+            }
+
+            if(transform.parent != currentGrid)
+            {
+                transform.parent = currentGrid;
+                currentGridRigidbody2D = currentGrid.GetComponent<Rigidbody2D>();
+            }
+        }
+
+        void TEMP_HandleGridMovement()
+        {
+            Debug.Log("wat..");
+            if(currentGrid != null)
+            {
+                Debug.Log(Input.GetKey(KeyCode.Space) + " aaaa");
+                Vector2 forceDirection = Input.GetKey(KeyCode.Space) ? Vector2.left * 5f * Time.deltaTime : Vector2.zero;
+                currentGridRigidbody2D.AddForce(forceDirection);
+                Debug.Log(currentGridRigidbody2D.velocity + " " + forceDirection);
+            }
         }
 
         void HandleMovement()
         {
-            Debug.Log(inputManager.movementInput);
             Vector2 localVelocity = inputManager.movementInput * movementSpeed;
             playerRigidbody.velocity = localVelocity;
+
+            if (currentGrid != null)
+            {
+                playerRigidbody.velocity += currentGridRigidbody2D.velocity;
+            }
         }
 
         void HandleRotation()
