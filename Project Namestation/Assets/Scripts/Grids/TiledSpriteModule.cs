7d2fe3cce4d890bebb3049607b19e76899e2cadf
Juš Dolžan <jus1671@gmail.com>
2022-01-18 17:29:52 +0100

    Minor changes, removed placeholder ui

---
+++ Project Namestation/Assets/Scripts/Grids/TiledSpriteModule.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiledSpriteModule : MonoBehaviour
+{
+    public Sprite standaloneSprite;
+    public Sprite cornerSprite;
+    public Sprite edgeSprite;
+    public Sprite straghtSprite;
+    public Sprite centerSprite;
+    public Sprite endSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
